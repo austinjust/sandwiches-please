70d8e3aff1a1cec5c500c75f933b99aa5fb940b0
austinjust <adjectivenoun@gmail.com>
2014-05-18 21:13:40 -0700

    i'm too dumb
    nothing nothing

--- spaceBall.js
+++ spaceBall.js
@@ -5,7 +5,7 @@ function l(what) {return document.getElementById(what);}
 var spaceBall = 0
 
 spaceBall.appear = function (); {
-			var me=l('#spaceBall');
+			var me=l('spaceBall');
 			me.body.style.backgroundPosition='center';*/
 			me.body.style.backgroundImage='url('moonjust.png')';
 			//var screen=l('game').getBoundingClientRect(); okay what is
@@ -13,7 +13,7 @@ spaceBall.appear = function (); {
 			//this.y=Math.floor(Math.random()*Math.max(0,screen.bottom-screen.top-128)+screen.top+64)-64;
 			//me.style.left=this.x+'px';
 			//me.style.top=this.y+'px';
-			me.style.display='block';
+				//me.style.display='block';
 			//how long will it stay on-screen?
 				//var dur=13;
 			//if (Life.Has('Lucky day')) dur*=2;
